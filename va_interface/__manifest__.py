010a276712bd81e797f1068b7b7a2c45c1e271ed
vertical-access <63242614+vertical-access@users.noreply.github.com>
2020-05-09 23:46:29 +0200

    [ADD] va_interface

---
+++ va_interface/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "va_interface",
+
+    'summary': """
+        vertical-access.ch module for custom import""",
+
+    'description': """
+        Used to process CSV file for custom imports
+    """,
+
+    'author': "Vertical Access Sàrl",
+    'website': "http://www.vertical-access.ch",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Vertical-Access',
+    'version': '0.1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        ],
+
+    # always loaded
+    'data': [
+        
+    ],
+
+}
---
+++ va_interface/models/res_partner.py
@@ -0,0 +1,5 @@
+import logging
+
+from odoo import api, fields, models, _
+
+_logger = logging.getLogger(__name__)
