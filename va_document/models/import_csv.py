4f45bed610ea9cd683b8090c35fff352321bd942
vertical-access <63242614+vertical-access@users.noreply.github.com>
2020-05-10 22:55:29 +0200

    Update import_csv.py

--- va_document/models/import_csv.py
+++ va_document/models/import_csv.py
@@ -15,12 +15,29 @@ class Document(models.Model):
         context = self.env.context
         if context.get('active_ids'):
             docs = self.browse(context['active_ids'])
-            for doc in docs.filtered(lambda d: self.env.ref('va_document.doctag_import_contact_pd_company') in d.tag_ids):
-                decoded_data = base64.b64decode(doc.attachment_id.datas)
-                data = io.StringIO(decoded_data.decode("utf-8"))
-                data.seek(0)
-                file_reader = []
-                csv_reader = csv.reader(data, delimiter=';')
-                file_reader.extend(csv_reader)
-                _logger.info("{}".format(file_reader))
+            for doc in docs:
+                if self.env.ref('va_document.doctag_import_contact_pd_company') in doc.tag_ids:
+                    doc.process_pipedrive_company(doc.csv_decode(';'))
+                else:
+                    pass
+    
+    @api.one
+    def csv_decode(self,delimiter=','):
+        decoded_data = base64.b64decode(self.attachment_id.datas)
+        data = io.StringIO(decoded_data.decode("utf-8"))
+        data.seek(0)
+        file_reader = []
+        csv_reader = csv.reader(data, delimiter=';')
+        file_reader.extend(csv_reader)
+        return file_reader
+    
+    @api.one
+    def process_pipedrive_company(self,data=False):
+        if data:
+            headers = data.pop(0)
+            for item in data:
+                vals = dict.fromkeys(headers,item)
+                _logger.info("{}".format(vals))
+
+
 
