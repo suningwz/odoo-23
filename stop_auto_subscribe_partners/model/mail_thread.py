173054dae4475e16afc50fd0b9e9329fd8438bb6
vertical-access <63242614+vertical-access@users.noreply.github.com>
2020-06-09 23:41:19 +0200

    Update mail_thread.py

--- stop_auto_subscribe_partners/model/mail_thread.py
+++ stop_auto_subscribe_partners/model/mail_thread.py
@@ -20,7 +20,7 @@ class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
 
     def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None):
-        _logger.info("CONTEXT | {}".format(self.env.context))
+        _logger.info("CONTEXT SUBSCRIBE | {}".format(self.env.context))
         ir_config = self.env['ir.config_parameter']
         app_stop_subscribe = bool(strtobool(ir_config.sudo().get_param('app_stop_subscribe')))
         if app_stop_subscribe:
@@ -29,6 +29,7 @@ class MailThread(models.AbstractModel):
             return super(MailThread, self).message_subscribe(partner_ids, channel_ids, subtype_ids)
 
     def _message_auto_subscribe(self, updated_fields):
+        _logger.info("CONTEXT AUTOSUBSCRIBE | {}".format(self.env.context))
         ir_config = self.env['ir.config_parameter']
         app_stop_subscribe = bool(strtobool(ir_config.sudo().get_param('app_stop_subscribe')))
         if app_stop_subscribe:
@@ -37,6 +38,7 @@ class MailThread(models.AbstractModel):
             return super(MailThread, self)._message_auto_subscribe(updated_fields)
 
     def _message_auto_subscribe_notify(self, partner_ids, template):
+        _logger.info("CONTEXT SUBSCRIBE NOTIFY | {}".format(self.env.context))
         ir_config = self.env['ir.config_parameter']
         app_stop_subscribe = bool(strtobool(ir_config.sudo().get_param('app_stop_subscribe')))
         if app_stop_subscribe:
