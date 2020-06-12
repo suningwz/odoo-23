2fbdaf8aa43ed3026a3dc25f4be405e9df867d23
vertical-access <63242614+vertical-access@users.noreply.github.com>
2020-06-13 01:00:09 +0200

    Update res_partner.py

--- va_contact/models/res_partner.py
+++ va_contact/models/res_partner.py
@@ -59,29 +59,35 @@ class ResPartner(models.Model):
     
     @api.onchange('social_reason_id')
     def _onchange_social_reason_id(self):
-        prev = self._origin
-        #we manage the case of setting a social reason
-        if self.is_company and self.social_reason_id and not prev.social_reason_id:
-            if self.social_reason_id.add_to_name:
-                new_name = self.with_context(lang=self.lang).social_reason_id.name
-                self.name = self.find_and_split(new_name,self.name) + " " + new_name
-        
-        # we remove the social reason
-        elif prev.social_reason_id and not self.social_reason_id:
-            old_name = prev.with_context(lang=self.lang).social_reason_id.name
-            self.name = self.find_and_split(old_name,self.name)
-        
-        else:
-            #normal change
-            old_name = prev.with_context(lang=self.lang).social_reason_id.name
-            new_name = self.with_context(lang=self.lang).social_reason_id.name
-            temp = self.find_and_split(old_name,self.name)
-            _logger.info("{} removed from {} output {}".format(old_name,self.name,temp))
-            if self.social_reason_id.add_to_name:
-                self.name = self.find_and_split(new_name,temp) + " " + new_name
-                _logger.info("{} added to {} output {}".format(new_name,temp,self.name))
+        if self.is_company:
+            prev = self._origin
+            #we manage the case of setting a social reason
+            if self.social_reason_id and not prev.social_reason_id:
+                _logger.info("New Case")
+                if self.social_reason_id.add_to_name:
+                    new_name = self.with_context(lang=self.lang).social_reason_id.name
+                    self.name = self.find_and_split(new_name,self.name) + " " + new_name
+            
+            # we remove the social reason
+            elif prev.social_reason_id and not self.social_reason_id:
+                _logger.info("Remove Case")
+                old_name = prev.with_context(lang=self.lang).social_reason_id.name
+                self.name = self.find_and_split(old_name,self.name)
+            
             else:
-                self.name = temp
+                _logger.info("Normal Case")
+                #normal change
+                old_name = prev.with_context(lang=self.lang).social_reason_id.name
+                new_name = self.with_context(lang=self.lang).social_reason_id.name
+                temp = self.find_and_split(old_name,self.name)
+                _logger.info("{} removed from {} output {}".format(old_name,self.name,temp))
+                if self.social_reason_id.add_to_name:
+                    self.name = self.find_and_split(new_name,temp) + " " + new_name
+                    _logger.info("{} added to {} output {}".format(new_name,temp,self.name))
+                else:
+                    self.name = temp
+        else:
+            pass
             
     def find_and_split(self,search=False,string=False):
         if search and string:
