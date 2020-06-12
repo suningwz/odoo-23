054b0d207b5c46a569b22152ba7c60d41ce21f22
vertical-access <63242614+vertical-access@users.noreply.github.com>
2020-06-13 00:30:58 +0200

    Social reason naming rework

--- va_contact/models/res_partner.py
+++ va_contact/models/res_partner.py
@@ -60,15 +60,36 @@ class ResPartner(models.Model):
     @api.onchange('social_reason_id')
     def _onchange_social_reason_id(self):
         prev = self._origin
-        if self.is_company and self.social_reason_id:
-            social_reason = (" " + self.with_context(lang=self.lang).social_reason_id.name)
-            if prev.social_reason_id:
-                prev_social_reason = (" " + prev.with_context(lang=self.lang).social_reason_id.name)
-                self.name = self.name.split(prev_social_reason)[0] + social_reason
-                _logger.info("Prev {} | New {}".format(prev_social_reason,social_reason))
-            else:
-                self.name = self.name + social_reason
-            
+        #we manage the case of setting a social reason
+        if self.is_company and self.social_reason_id and not prev.social_reason_id:
+            if self.social_reason_id.add_to_name:
+                new_name = self.with_context(lang=self.lang).social_reason_id.name
+                self.name = self.find_and_split(new_name,self.name) + " " + new_name
+        
+        # we remove the social reason
+        elif prev.social_reason_id and not self.social_reason_id:
+            old_name = prev.with_context(lang=self.lang).social_reason_id.name
+            self.name = self.find_and_split(old_name,self.name)
+        
         else:
-            pass
+            #normal change
+            old_name = prev.with_context(lang=self.lang).social_reason_id.name
+            new_name = self.with_context(lang=self.lang).social_reason_id.name
+            temp = self.find_and_split(old_name,self.name)
+            if self.social_reason_id.add_to_name:
+                self.name = temp + " " + new_name
+            else:
+                self.name = temp
+            
+    def find_and_split(self,search=False,string=False):
+        if search and string:
+            found = string.lower().search(search.lower()) 
+            if found > 0:
+                output = string[:found] + string[found+len(search):]
+                _logger.info("{} found in {} output {}".format(search,string,output))
+                return output
+            else:
+                return string
+        else:
+            return string
 
