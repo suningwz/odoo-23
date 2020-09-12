47eaa360fa25990cc8070c281b4976af3d7401de
vertical-access <63242614+vertical-access@users.noreply.github.com>
2020-09-12 22:59:09 +0200

    ref in SO name

--- va_sales/__manifest__.py
+++ va_sales/__manifest__.py
@@ -9,14 +9,14 @@
         Long description of module's purpose
     """,
 
-    'author': "Vertical Access",
+    'author': "Vertical Access Sàrl",
     'website': "http://www.vertical-access.ch",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Vertical-Access',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': [
