ebdc72a260c7c797ae6098ae0504ca918ab54511
vertical-access <63242614+vertical-access@users.noreply.github.com>
2020-09-12 23:45:09 +0200

    Chatter in project Form

--- va_project/__manifest__.py
+++ va_project/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Vertical-Access',
-    'version': '0.2',
+    'version': '0.4',
 
 
     # any module necessary for this one to work correctly
@@ -30,7 +30,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/project_views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
