bb705727aac812c438e4c97cf41ebefb9ceff63c
vertical-access <63242614+vertical-access@users.noreply.github.com>
2020-09-21 23:29:12 +0200

    Create project with default tasks stage

--- va_project/models/project.py
+++ va_project/models/project.py
@@ -10,3 +10,11 @@ class Project(models.Model):
         'mail.activity.mixin',
         ]
 
+    @api.model
+    def create(self, vals):
+        project_type_id = vals.get('type_id',False)
+        ids = self.env["project.task.type"].search([("case_default", "=", True),("default_project_type_id", "=", project_type_id)])
+        if ids:
+            vals['type_ids'] = [(6,0,ids)]
+        return super(Project, self).create(vals)
+
