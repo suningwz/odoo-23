9bb57198ab2299c4897be90922ed943da93096ce
vertical-access <63242614+vertical-access@users.noreply.github.com>
2020-09-21 23:41:22 +0200

    fix

--- va_project/models/project.py
+++ va_project/models/project.py
@@ -13,8 +13,8 @@ class Project(models.Model):
     @api.model
     def create(self, vals):
         project_type_id = vals.get('type_id',False)
-        ids = self.env["project.task.type"].search([("case_default", "=", True),("default_project_type_id", "=", project_type_id)])
-        if ids:
-            vals['type_ids'] = [(6,0,ids)]
+        stages = self.env["project.task.type"].search([("case_default", "=", True),("default_project_type_id", "=", project_type_id)])
+        if stages:
+            vals['type_ids'] = [(6,0,stages.ids)]
         return super(Project, self).create(vals)
 
