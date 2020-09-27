4a870e4401d6eb012a24b2a5adb94733f9a4f44e
vertical-access <63242614+vertical-access@users.noreply.github.com>
2020-09-27 23:20:25 +0200

    quantity change doesn't change the price

--- va_sales/models/sale.py
+++ va_sales/models/sale.py
@@ -26,3 +26,23 @@ class SaleOrder(models.Model):
         vals['name'] = "{} | {}".format(vals['seq_ref'],vals['name'])
         result = super(SaleOrder, self).create(vals)
         return result
+    
+    #we override this because we don't want the price to be updated anymore when we change the ordered qty
+    #@api.onchange('product_uom', 'product_uom_qty')
+    @api.onchange('product_uom')
+    def product_uom_change(self):
+        super(SaleOrder, self).product_uom_change()
+        """if not self.product_uom or not self.product_id:
+            self.price_unit = 0.0
+            return
+        if self.order_id.pricelist_id and self.order_id.partner_id:
+            product = self.product_id.with_context(
+                lang=self.order_id.partner_id.lang,
+                partner=self.order_id.partner_id,
+                quantity=self.product_uom_qty,
+                date=self.order_id.date_order,
+                pricelist=self.order_id.pricelist_id.id,
+                uom=self.product_uom.id,
+                fiscal_position=self.env.context.get('fiscal_position')
+            )
+            self.price_unit = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)"""
