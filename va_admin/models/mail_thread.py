ba716f61b76df3372bbd72e6fd116c7bf58b6285
vertical-access <63242614+vertical-access@users.noreply.github.com>
2021-03-23 21:20:03 +0100

    Test install V14

--- va_admin/models/mail_thread.py
+++ va_admin/models/mail_thread.py
@@ -8,9 +8,9 @@ _logger = logging.getLogger(__name__)
 class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
 
-    def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None):
+    """def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None):
         #we avoid autosubscription from the message, but only from the add follower menu
         if not ('mail_invite_follower_channel_only' in self._context):
             return
         else:
-            return super(MailThread, self).message_subscribe(partner_ids, channel_ids, subtype_ids)
+            return super(MailThread, self).message_subscribe(partner_ids, channel_ids, subtype_ids)"""
