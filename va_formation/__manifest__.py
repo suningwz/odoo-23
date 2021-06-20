3aba8b0ee432eca4681abbd73be980656c796562
vertical-access <63242614+vertical-access@users.noreply.github.com>
2021-06-20 18:47:57 +0200

    Update __manifest__.py

--- va_formation/__manifest__.py
+++ va_formation/__manifest__.py
@@ -16,12 +16,13 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Vertical-Access',
-    'version': '14.0.21',
+    'version': '14.0.22',
 
     # any module necessary for this one to work correctly
     'depends': [
         'base',
         'event',
+        'website_event',
         ],
 
     # always loaded
