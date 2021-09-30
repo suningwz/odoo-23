bc09df7f9acb96a991b479d5ea287044c12bff43
vertical-access <63242614+vertical-access@users.noreply.github.com>
2021-09-30 23:28:05 +0200

    Update __manifest__.py

--- safety_building/__manifest__.py
+++ safety_building/__manifest__.py
@@ -16,10 +16,13 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Services',
-    'version': '14.0.4',
+    'version': '14.0.5',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'base',
+        'mail',
+        ],
 
     # always loaded
     'data': [
